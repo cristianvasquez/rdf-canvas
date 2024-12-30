1703b9d44d36c2682e972810115a0c4fa18ad083
Cristian <cristianvasquez@gmail.com>
2024-12-30 23:26:44 +0100

    entities instead

--- src/app/components/FlowNode.vue
+++ src/app/components/FlowNode.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { Handle, Position } from '@vue-flow/core'
-import Term from './Term.vue'
-import Row from './Row.vue'
+import Entity from './Entity.vue'
 
 const props = defineProps({
   data: Object,
@@ -22,60 +21,18 @@ const entityData = {
   <div class="flow-node" :data-id="props.id">
     <Handle type="source" :position="Position.Right"/>
 
-    <div class="entity">
-      <div class="entity-header">
-        <Term :term="entityData.term"/>
-      </div>
-
-      <div class="rows">
-        <template v-for="row in entityData.rows" :key="row.predicate.value">
-          <Row :row="row"/>
-        </template>
-      </div>
-    </div>
-
+    <Entity :pointer="entityData"/>
     <Handle type="target" :position="Position.Left"/>
   </div>
 </template>
 
 <style>
+
 .flow-node {
-  min-width: 300px;
+  min-width: 600px;
   border-right: 1px solid var(--border);
 }
 
-.flow-node .entity {
-  display: flex;
-  flex-direction: column;
-  border-top: 1px solid var(--border);
-  border-left: 1px solid var(--border);
-  border-bottom: 1px solid var(--border);
-  background: white;
-}
-
-.flow-node .entity-header {
-  background: rgba(0, 0, 0, 0.05);
-}
-
-.flow-node .entity-header > div {
-  margin: 5px;
-}
-
-.flow-node .rows {
-  display: flex;
-  flex-direction: column;
-}
-
-.flow-node .rows > :nth-child(1n) {
-  border-top: 1px solid var(--border);
-}
-
-.rows > :nth-child(2n) {
-  border-top: 1px solid var(--border);
-  background: rgba(0, 0, 0, 0.01);
-}
-
-/* Handle styling */
 .vue-flow__handle {
   width: 8px;
   height: 8px;
